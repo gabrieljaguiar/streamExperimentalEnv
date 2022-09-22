72a72d51d0f0566010b9788865ab3bfae00456a5
Gabriel Jonas <gja.gbrl@gmail.com>
2022-09-22 15:32:23 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+
+#ideal would be something like
+#evaluator = Evaluator (window_size = 500, metrics = X)
+#exp = Experiment(evaluator, number_of_parallel = 8, output_folder = X)
+#exp.add(classifier, stream, name = "X")
+#exp.add(classifier, stream, name = "Y")
+#exp.add(classifier, stream, name = "Z")
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
