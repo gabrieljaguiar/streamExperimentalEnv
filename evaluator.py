d1643e78685084b866957e5811f4892a2011ee26
gabrieljaguiar <gja.gbrl@gmail.com>
2022-09-23 10:53:57 -0400

    evaluator and experiment

--- evaluator.py
+++ evaluator.py
@@ -1,3 +1,36 @@
+from river import metrics
+
+
 class Evaluator():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, window_size = 500, metric_list = None) -> None:
+        self.windowMetrics = []
+        
+
+        self.metrics = {
+            "accuracy": metrics.Accuracy,
+            "kappa": metrics.CohenKappa,
+            "gmean": metrics.GeometricMean,
+            "cm": metrics.ConfusionMatrix
+        }
+
+        if (metric_list is None):
+            metric_list = self.metrics.keys()
+
+        self.metric_list = metric_list
+        
+        for m in metric_list:
+            metric_function = self.metrics.get(m)
+            self.windowMetrics.append([m, metric_function()])
+
+
+
+    def add(self, trueClass: int, predictedClass: int) -> None:
+        for m in self.windowMetrics:
+            m[1].update(trueClass, predictedClass)
+    
+
+    def reset(self):
+        self.windowMetrics = []
+        for m in self.metric_list:
+            metric_function = self.metrics.get(m)
+            self.windowMetrics.append([m, metric_function()])
--- experiment.py
+++ experiment.py
@@ -1,3 +1,18 @@
+from evaluator import Evaluator
+from river.base import Classifier
+from river.datasets.base import Dataset
+
 class Experiment():
-    def __init__(self) -> None:
+    def __init__(self, eval: Evaluator, output_folder:str, number_of_processes: int = 1) -> None:
+        self.evaluator = eval
+        self.output_folder = output_folder
+        self.number_of_processes = number_of_processes
+        self.experiments = []
+
+
+    def add(self, model: Classifier, data: Dataset, exp_name: str) -> None:
+        self.experiments.append((model, data, exp_name))
+    
+
+    def run() ->  None:
         pass
